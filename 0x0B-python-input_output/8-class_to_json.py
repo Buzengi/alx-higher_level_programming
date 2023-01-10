9e04fa899a8c9fbd570771eac4276c8e9aef087d
Chidi <ajaerohc@gmail.com>
2023-01-10 22:43:13 +0000

    function that returns dictionary description with simple data structure for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a JSON file-reading function."""
-import json
+"""This module defines a Python class-to-JSON function"""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dict__
